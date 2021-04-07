5c8f475a76e7b7623a03532f4a239e222bbda63a
hermitty <wiktoria.pustelniak@gmail.com>
2021-04-07 14:49:24 +0200

    Init service

---
+++ feed_me/lib/service/food_service.dart
@@ -0,0 +1,10 @@
+import 'package:feed_me/model/food.dart';
+import 'package:feed_me/model/tag.dart';
+
+class FoodService {
+  void addFood(Food food) {}
+  void editFood(Food food) {}
+  void deleteFood(int foodId) {}
+  List<Food> getFoodByTags(List<Tag> tags) { return <Food>[]; }
+  List<Food> getFood() { return <Food>[]; }
+}
